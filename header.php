70c817ecaa6011814cb5a2908efb71ef44ed9c7d
Dan Lazin <dlazin@dlazin-macbookpro2.roam.corp.google.com>
2016-11-27 14:13:19 -0500

    Inline theme CSS and remove analytics in favor of CAOS local-analytics-hosting plugin

--- header.php
+++ header.php
@@ -3,19 +3,10 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,400italic,700">
-		<link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+		<style><?php echo file_get_contents( get_stylesheet_uri() ); ?></style>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
-		<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-		ga('create', 'UA-49042255-1', 'auto');
-		ga('send', 'pageview');
-		</script>
 		<div class="ok-wrapper">
 			<nav class="ok-nav">
 				<a href="#" class="ok-menu-control" onclick="document.body.classList.toggle('ok-menu-visible')">

51c267ae15ceb07ebba60df023beab353c634e2e
Dan Lazin <dlazin@dlazin-macbookpro2.roam.corp.google.com>
2016-11-27 14:18:59 -0500

    Revert local font CSS hosting

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,400italic,700">
 		<style><?php echo file_get_contents( get_stylesheet_uri() ); ?></style>
 		<?php wp_head(); ?>
 	</head>

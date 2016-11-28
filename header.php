eba6a1ffaf352fb74627e1d7dfe234e56310fbe1
Dan Lazin <dlazin@dlazin-macbookpro2.roam.corp.google.com>
2016-11-28 01:01:35 -0500

    Use include instead of file_get_contents to avoid cache headers

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,400italic,700">
-		<style><?php echo file_get_contents( get_stylesheet_uri() ); ?></style>
+		<style><?php include 'style.css' ?></style>
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>

e6f12e658c53d06269dffef6ab4397cbec11ca05
Dan Lazin <dlazin@dlazin-macbookpro2.roam.corp.google.com>
2016-12-10 17:50:17 -0500

    Adjust loop formatting

--- loop.php
+++ loop.php
@@ -1,12 +1,13 @@
 <?php
-if ( have_posts() ) :
+if ( have_posts() ) {
 	$count = 0;
-	while ( have_posts() ) : the_post(); 
+	while ( have_posts() ) {
+		the_post(); 
 		$count++;
-		if ($count == 3) {
+		if ( $count == 3 ) {
 			get_template_part( 'ad', 'middle' );
 		}
 		get_template_part( 'entry' );
-	endwhile;
-endif; 
+	}
+}
 ?>

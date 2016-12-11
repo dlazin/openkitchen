db6e644f2760c7f0fd6fcda2c8daa11abc47cb19
Dan Lazin <dlazin@dlazin-macbookpro2.roam.corp.google.com>
2016-12-11 11:33:53 -0500

    Link post thumbnails on index pages

--- entry.php
+++ entry.php
@@ -2,6 +2,16 @@
 	<?php if ( is_singular() ) { echo '<h1 class="ok-title">'; } else { echo '<h2 class="ok-title">'; } ?>
 		<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" rel="bookmark"><?php the_title(); ?></a>
 	<?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
-	<?php if ( has_post_thumbnail() ) { the_post_thumbnail( 'large', array('class' => 'aligncenter') ); } ?>
+	<?php if ( has_post_thumbnail() ) {
+		if ( !is_singular() ) {
+			echo( '<a href="' );
+			the_permalink();
+			echo( '">' );
+		}
+		the_post_thumbnail( 'large', array('class' => 'aligncenter') );
+		if ( !is_singular() ) {
+			echo( '</a>' );
+		}
+	} ?>
 	<?php the_content( $more_link_text = 'Keep reading' ); ?>
 </article>
